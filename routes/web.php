e594c30f9dd10ff89e44fa2c1c2e3f142ca4e600
Anisha01-1 <152585934+Anisha01-1@users.noreply.github.com>
2025-06-23 13:01:24 +0545

    Add files via upload
    route

--- routes/web.php
+++ routes/web.php
@@ -2,33 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Patient\PatientController;
 use App\Http\Controllers\Auth\RegistrationController;
+use App\Http\Controllers\Caregiver\ProfileController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Caregiver\CaregiverController;
 
-Route::get('/forgotpassword', function () {
-    return view('auth.forgotpassword');
-});
-
-Route::get('/fregister', function () {
-    return view('auth.register');
-});
+Route::get('/Caregiver/dashboard', [CaregiverController::class, 'index'])->name('caregiver.dashboard');
+Route::get('/Patient/dashboard', [PatientController::class, 'index'])->name('patient.dashboard');
 
 
-Route::get('/resetpassword', function () {
-    return view('auth.resetpassword');
-});
+Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+Route::get('/admin/profile', [DashboardController::class, 'profile'])->name('admin.profile');
 
+Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('Caregiver.edit');
+Route::post('/profile/update', [ProfileController::class, 'update'])->name('Caregiver.update');
 
-
-
-// Route::get('/register', [RegistrationController::class,'register'])->name('auth.register'); 
-// Route::post('/registerSave', [RegistrationController::class,'register'])->name('auth.registerSave');
-
-Route::get('/forgetpassword', [ForgotPasswordController::class,'forgetpassword'])->name('auth.forgetpassword');
-// Route::get('/resetpassword', [RegistrationController::class,'resetpassword'])->name('auth.resetpassword');
-// Route::get('/index', [RegistrationController::class,'index'])->name('dashboard.index');
-
-// Route::get('/admin/dashboard', [RegistrationController::class,'register'])->name('auth.register');
+// Route::get('/edit', function () {
+//     return view('caregiver.edit');
+// });
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('auth.login');
 Route::post('/login', [LoginController::class, 'doLogin'])->name('auth.doLogin');
@@ -36,3 +29,5 @@ Route::post('/login', [LoginController::class, 'doLogin'])->name('auth.doLogin')
 
 Route::get('/register', [RegistrationController::class, 'showForm'])->name('auth.registerForm');
 Route::post('/register', [RegistrationController::class, 'registerSave'])->name('auth.register');
+
+Route::get('/forgetpassword', [ForgotPasswordController::class,'forgetpassword'])->name('auth.forgetpassword');
